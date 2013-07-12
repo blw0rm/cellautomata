83c65edc5aecffe67e1be0005c673a7ad4765b06
Yaroslav D <blw0rm@gmail.com>
2013-07-12 13:25:34 +0200

    A bit of refactoring

--- README.md
+++ README.md
@@ -2,14 +2,16 @@ Cellular Automata
 =================
 
 This is an example implementation of cellular automaton algorithm.
-It's short description can be found at [The Mendicant Bug](http://mendicantbug.com/2007/10/28/simple-cellular-automata/ "The Mendicant Bug")
-More information about algorithm can be found at [The Mendicant Bug] (http://en.wikipedia.org/wiki/Cellular_automaton "The Mendicant Bug")
+It's short description can be found at [The Mendicant Bug](http://mendicantbug.com/2007/10/28/simple-cellular-automata/ "The Mendicant Bug").
+More information about algorithm can be found at [Wikipedia] (http://en.wikipedia.org/wiki/Cellular_automaton "Wikipedia").
 
 Usage:
-$ chmod +x ca.py
-$ ca.py <initial data> <neighbourhood bits> <rules set>
+
+    $ chmod +x ca.py
+    $ ca.py <initial data> <neighbourhood bits> <rules set>
 
 Assumptions:
+
 1. nbits - bits number per neighborhood, i.e. 3;
 2. data - Initial data which is a number, exponent of two, i.e 1099511627776 (0x10000000000);
 3. ruleset - Rules set, 8 bit, i.e. 90;
--- ca.py
+++ ca.py
@@ -7,6 +7,7 @@ Cellular Automata
 This is an example implementation of cellular automata algorithm.
 """
 
+import sys
 import math
 
 __author__ = "Yaroslav Dashivets"
@@ -20,36 +21,41 @@ __email__ = "yaroslav.dashivets@gmail.com"
 __status__ = "Prototype"
 
 class CA(object):
-    def __init__(self, data, nbits, ruleset, iterations):
+    def __init__(self, data, nbits, ruleset):
         """
-        Cellular Automata implementation.
+        Cellular Automata implementation. Accepts next arguments:
+            * data - Initial data, should be exponent of 2 number;
+            * nbits - odd number of bits per neighbourhood, i.e 3,5,7;
+            * ruleset - integer number, which is a set of rules, e.g. 90.
         """
+        assert int(nbits) % 2, "Number or bits per neighbourhood should be odd"
+        assert (math.log(data, 2)*2+1).is_integer(), "Initial data should be exponent of two"
         self.data = data
         self.nbits = nbits
         self.ruleset = ruleset
-        self.niters = iterations
 
     def get_ca_size(self):
         """Calculates cellular automata size"""
         if not hasattr(self, "ca_size"):
-            self.ca_size = math.log(self.data, 2)*2+1
+            self.ca_size = int(math.log(self.data, 2)*2+1)
         return self.ca_size
 
     def apply_rule(self, ng):
         """
         Applies appropriate rule to the neighbour group.
+        Returns 0 or 1 rule can be applied.
         """
         return int(bool(self.ruleset & 2**ng))
 
     def get_ng(self, row, index):
         """Returns neighbours group by the index"""
-        num_neighbours = (self.nbits - 1) / 2
+        num_neighbours = (self.nbits - 1) / 2  # number of one-way neighbours
         row_size = int(self.get_ca_size())
-        if index in (0, row_size - 1):
+        if index in (0, row_size - 1):  # direction <--
             if index == 0:
                 shift = num_neighbours
                 base = self.nbits - num_neighbours
-            elif index == row_size - 1:
+            else:
                 shift = self.nbits - num_neighbours
                 base = num_neighbours
 
@@ -65,7 +71,7 @@ class CA(object):
         print_result = ""
         for i in xrange(self.get_ca_size()):
             processed_cell = self.apply_rule(self.get_ng(row, i))
-            result = result | processed_cell
+            result |= processed_cell
             if i < self.get_ca_size() - 1:
                 result <<= 1
             print_result += str(processed_cell)
@@ -74,8 +80,10 @@ class CA(object):
         return result
 
     def iterate(self, iterations):
-        """Iterates n times"""
+        """Iterates 'iterations' times"""
         row = self.data
         bin_row = bin(row)[2:]
-        for i in xrange(iterations):
+        print "0"*(self.get_ca_size() - len(bin_row))+bin_row
+        while iterations:
             row = self.process_data(row)
+            iterations -= 1
